546df25749b8d3a8195d6d1e94dba2169ebf725c
ryouhama <hary0730cap@icloud.com>
2022-03-27 18:24:37 +0900

    add Home

---
+++ ui/src/pages/Home/container.tsx
@@ -0,0 +1,8 @@
+import { HomePresenter as Home } from './persenter'
+
+type Props = {}
+
+export const HomeContainer: React.FC<Props> = (props) => {
+
+  return <Home></Home>
+}
---
+++ ui/src/pages/Home/persenter.tsx
@@ -0,0 +1,10 @@
+type Props = {}
+
+export const HomePresenter: React.FC<Props> = (props) => {
+
+  return (
+    <div>
+      Welcome to Ui Components
+    </div>
+  )
+}
